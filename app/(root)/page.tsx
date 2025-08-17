c9cf5c1cc06232a433e6d10e8c6f92ff17f6fb05
Safghan Alam <khansafgan1743@gmail.com>
2025-08-17 15:39:38 +0530

    Fix Project

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl">HOME</h1>
+      <h1 className="heading">HOME</h1>
     </>
   );
 }
